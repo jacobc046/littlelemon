238ee43a1cd176c6518ab2c1cced86153c9068a9
Jacob Croket <thisisjacobseml@icloud.com>
2024-06-25 13:01:41 -0400

    Uploading project

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Jacob Croket on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
